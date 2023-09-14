f43128dd33b746bd8d5b54df2917e4d6f88465a9
Mészáros Balázs <58610433+fireball90@users.noreply.github.com>
2023-09-14 10:41:44 +0200

    Add files via upload

---
+++ BoxingGame/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace BoxingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.enemyLife = new System.Windows.Forms.ProgressBar();
+            this.playerLife = new System.Windows.Forms.ProgressBar();
+            this.enemyBoxer = new System.Windows.Forms.PictureBox();
+            this.boxer = new System.Windows.Forms.PictureBox();
+            this.enemyTimer = new System.Windows.Forms.Timer(this.components);
+            this.enemyMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyBoxer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // enemyLife
+            // 
+            this.enemyLife.Location = new System.Drawing.Point(39, 44);
+            this.enemyLife.Name = "enemyLife";
+            this.enemyLife.Size = new System.Drawing.Size(300, 25);
+            this.enemyLife.TabIndex = 0;
+            this.enemyLife.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // playerLife
+            // 
+            this.playerLife.Location = new System.Drawing.Point(476, 44);
+            this.playerLife.Name = "playerLife";
+            this.playerLife.Size = new System.Drawing.Size(300, 25);
+            this.playerLife.TabIndex = 1;
+            // 
+            // enemyBoxer
+            // 
+            this.enemyBoxer.BackColor = System.Drawing.Color.Transparent;
+            this.enemyBoxer.Image = global::BoxingGame.Properties.Resources.enemy_stand;
+            this.enemyBoxer.Location = new System.Drawing.Point(357, 248);
+            this.enemyBoxer.Name = "enemyBoxer";
+            this.enemyBoxer.Size = new System.Drawing.Size(77, 185);
+            this.enemyBoxer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.enemyBoxer.TabIndex = 2;
+            this.enemyBoxer.TabStop = false;
+            // 
+            // boxer
+            // 
+            this.boxer.BackColor = System.Drawing.Color.Transparent;
+            this.boxer.Image = global::BoxingGame.Properties.Resources.boxer_stand;
+            this.boxer.Location = new System.Drawing.Point(365, 343);
+            this.boxer.Name = "boxer";
+            this.boxer.Size = new System.Drawing.Size(61, 153);
+            this.boxer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.boxer.TabIndex = 3;
+            this.boxer.TabStop = false;
+            // 
+            // enemyTimer
+            // 
+            this.enemyTimer.Enabled = true;
+            this.enemyTimer.Interval = 1000;
+            this.enemyTimer.Tick += new System.EventHandler(this.enemyPunchEvent);
+            // 
+            // enemyMove
+            // 
+            this.enemyMove.Enabled = true;
+            this.enemyMove.Interval = 20;
+            this.enemyMove.Tick += new System.EventHandler(this.enemyMoveEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::BoxingGame.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(825, 565);
+            this.Controls.Add(this.boxer);
+            this.Controls.Add(this.enemyBoxer);
+            this.Controls.Add(this.playerLife);
+            this.Controls.Add(this.enemyLife);
+            this.Name = "Form1";
+            this.Text = "Boxing Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyBoxer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boxer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar enemyLife;
+        private System.Windows.Forms.ProgressBar playerLife;
+        private System.Windows.Forms.PictureBox enemyBoxer;
+        private System.Windows.Forms.PictureBox boxer;
+        private System.Windows.Forms.Timer enemyTimer;
+        private System.Windows.Forms.Timer enemyMove;
+    }
+}
+
---
+++ BoxingGame/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxingGame
+{
+    public partial class Form1 : Form
+    {
+        bool blockAttack = false;
+
+        List<string> enemyAttacks = new List<string> { "left", "right", "block" };
+        Random rnd = new Random();
+
+        int enemySpeed = 5;
+        int i = 0;
+        bool enemyBlocked;
+
+        int playerHealth = 100;
+        int enemyHealth = 100; 
+        public Form1()
+        {
+            InitializeComponent();
+            playerLife.ForeColor = Color.Blue;
+            enemyLife.ForeColor = Color.Red; //nem működik pls
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enemyMoveEvent(object sender, EventArgs e)
+        {
+            enemyBoxer.Left += enemySpeed; 
+            //életek
+            if (playerHealth > 1)
+            {
+                playerLife.Value = Convert.ToInt32(playerHealth);
+            }
+
+            if (enemyHealth > 1)
+            {
+                enemyLife.Value = Convert.ToInt32(enemyHealth);
+            }
+
+
+            //ellenfél mozgás
+            if (enemyBoxer.Left > 480)
+            {
+                enemySpeed = -5;
+            }
+            if (enemyBoxer.Left < 315)
+            {
+                enemySpeed = 5;
+            }
+            //életek
+            if (enemyHealth < 1)
+            {
+                enemyTimer.Stop();
+                enemyMove.Stop();
+                MessageBox.Show("Nyertél! Kattinst egy új körért!");
+                
+                resetGame();
+            }
+
+            if (playerHealth < 1)
+            {
+                enemyTimer.Stop();
+                enemyMove.Stop();
+                MessageBox.Show("Kemény Józsi Nyert! Kattints egy új körért!");
+
+                resetGame();
+            }
+        }
+
+        private void enemyPunchEvent(object sender, EventArgs e)
+        {
+            i = rnd.Next(0, enemyAttacks.Count);
+
+            switch (enemyAttacks[i].ToString())
+            {
+                //bal oldal
+                case "left":
+                    enemyBoxer.Image = Properties.Resources.enemy_punch1;
+
+                    if (enemyBoxer.Bounds.IntersectsWith(boxer.Bounds) && !blockAttack)
+                    {
+                        playerHealth -= 20;
+                    }
+                    enemyBlocked = false; 
+                    break;
+
+                //jobb oldal
+                case "right":
+                    enemyBoxer.Image = Properties.Resources.enemy_punch2;
+
+                    if (enemyBoxer.Bounds.IntersectsWith(boxer.Bounds) && !blockAttack)
+                    {
+                        playerHealth -= 20;
+                    }
+                    enemyBlocked = false;
+                    break;
+
+                //block
+                case "block":
+
+                    enemyBoxer.Image = Properties.Resources.enemy_block;
+
+                    enemyBlocked = true;
+                    break;
+            }
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                boxer.Image = Properties.Resources.boxer_block;
+                blockAttack = true;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                boxer.Image = Properties.Resources.boxer_left_punch;
+
+                if (enemyBoxer.Bounds.IntersectsWith(boxer.Bounds) && !enemyBlocked)
+                {
+                    enemyHealth -= 5;
+                }
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                boxer.Image = Properties.Resources.boxer_right_punch;
+
+                if (enemyBoxer.Bounds.IntersectsWith(boxer.Bounds) && !enemyBlocked)
+                {
+                    enemyHealth -= 5;
+                }
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            boxer.Image = Properties.Resources.boxer_stand;
+            blockAttack = false;
+        }
+        private void resetGame()
+        {
+            enemyTimer.Start();
+            enemyMove.Start(); 
+
+            enemyBoxer.Left = 357; 
+            enemyBoxer.Top = 248; 
+
+            enemyBoxer.Image = Properties.Resources.enemy_stand; 
+            boxer.Image = Properties.Resources.boxer_stand; 
+
+            playerHealth = 100; 
+            enemyHealth = 100;
+        }
+    }
+}
